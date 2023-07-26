b9a7e98a006c0342644e8a4c5ecca86289692bac
Leandro Artilha <leandro.artilha@gmail.com>
2023-07-26 00:34:14 -0300

    add chakara ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Skeleton, Stack } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
       <h1>E-Commerce</h1>
+      <Button colorScheme="blue">Button</Button>
+      <Stack>
+        <Skeleton height="20px" />
+        <Skeleton height="20px" />
+        <Skeleton height="20px" />
+      </Stack>
+      {/* <Button
+        isLoading
+        colorScheme="blue"
+        spinner={<BeatLoader size={8} color="white" />}
+      >
+        Click me
+      </Button> */}
+      <Box color="gray.50" />
     </>
   );
 }
